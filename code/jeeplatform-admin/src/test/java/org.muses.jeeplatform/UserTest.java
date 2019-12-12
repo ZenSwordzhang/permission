da978c992e35052595e9751b9d08537862bd4614
zsx <18273117003@163.com>
2019-12-12 16:26:31 +0800

    modify

---
+++ code/jeeplatform-admin/src/test/java/org.muses.jeeplatform/UserTest.java
@@ -0,0 +1,22 @@
+//package org.muses.jeeplatform;
+//
+//import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.extension.ExtendWith;
+//import org.muses.jeeplatform.bean.User;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit.jupiter.SpringExtension;
+//
+//@ExtendWith(SpringExtension.class)
+//@SpringBootTest
+//public class UserTest {
+//
+//    @Autowired
+//    User user;
+//
+//    @Test
+//    void testConfigurationProperties(){
+//        System.out.println(user);
+//    }
+//
+//}
